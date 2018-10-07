4beb09bbeb2fddff4b299cb201aa0b052b9d0e0d
Nao Takegawa <nao.takegawa316@gmail.com>
2018-10-08 01:49:29 +0900

    create foundation of redux data management

--- src/components/PonyNote.js
+++ src/components/PonyNote.js
@@ -1,21 +1,56 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
-import {Link} from 'react-router-dom';
+import {notes} from "../actions";
+
 
 class PonyNote extends Component {
+
+  state = {
+    text: "",
+    updateNoteId: null,
+  }
+
+  resetForm = () => {
+    this.setState({text: "", updateNoteId: null});
+  }
+
+  selectForEdit = (id) => {
+    let note = this.props.notes[id];
+    this.setState({text: note.text, updateNoteId: id});
+  }
+
+  submitNote = (e) => {
+    e.preventDefault();
+    if (this.state.updateNoteId === null) {
+      this.props.addNote(this.state.text);
+    } else {
+      this.props.updateNote(this.state.updateNoteId, this.state.text);
+    }
+    this.resetForm();
+  }
+
   render() {
     return (
       <div>
         <h2>Welcome to PonyNote!</h2>
         <hr />
         <h3>Notes</h3>
+        <h4>Add new note</h4>
+        <form onSubmit={this.submitNote}>
+          <input
+           value={this.state.text}
+           placeholder="Enter note here..."
+           onChange={(e) => this.setState({text: e.target.value})}
+           required />
+          <input type="submit" value="Save Note" />
+        </form>
         <table>
           <tbody>
-            {this.props.notes.map(note => (
+            {this.props.notes.map((note, index) => (
               <tr>
                 <td>{note.text}</td>
-                <td><button>edit</button></td>
-                <td><button>delete</button></td>
+                <td><button onClick={() => this.selectForEdit(index)}>edit</button></td>
+                <td><button onClick={() => this.props.deleteNote(index)}>delete</button></td>
               </tr>
             ))}
           </tbody>
@@ -32,7 +67,17 @@ const mapStateToProps = state => {
 }
 
 const mapDispatchToProps = dispatch => {
-  return {}
+  return {
+    addNote: (text) => {
+      dispatch(notes.addNote(text));
+    },
+    updateNote: (id, text) => {
+      dispatch(notes.updateNote(id, text));
+    },
+    deleteNote: (id) => {
+      dispatch(notes.deleteNote(id));
+    },
+  }
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(PonyNote);
--- src/reducers/notes.js
+++ src/reducers/notes.js
@@ -14,6 +14,7 @@ export default function notes(state=initialState, action) {
       let noteToUpdate = noteList[action.id];
       noteToUpdate.text = action.text;
       noteList.splice(action.id, 1, noteToUpdate);
+      console.log(noteList)
       return noteList;
 
     case "DELETE_NOTE":
@@ -22,5 +23,27 @@ export default function notes(state=initialState, action) {
 
     default:
       return state;
+  }
+}
+
+export const addNote = text => {
+  return {
+    type: 'ADD_NOTE',
+    text
+  }
+}
+
+export const updateNote = (id, text) => {
+  return {
+    type: 'UPDATE_NOTE',
+    id,
+    text
+  }
+}
+
+export const deleteNote = id => {
+  return {
+    type: 'DELETE_NOTE',
+    id
   }
 }
