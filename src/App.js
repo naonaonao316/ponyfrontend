be0528181c9d57437b56294f2dc8e03549a17496
Nao Takegawa <nao.takegawa316@gmail.com>
2018-10-07 23:20:20 +0900

    set foundation of routing

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import React, { Component } from 'react';
 import { Route, Switch, BrowserRouter } from 'react-router-dom';
+import PonyNote from "./components/PonyNote";
+import { NotFound } from "./components/NotFound";
+
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={PonyNote} />
+          <Route component={NotFound} />
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
---
+++ src/components/NotFound.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const NotFound = () => {
+  return (
+    <div>
+      <h2>Not Found</h2>
+      <p>The page you're looking for does not exists.</p>
+    </div>
+  )
+}
